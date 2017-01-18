213ce6b27dcff882251b922306c6421efb23d7e0
MarvinThomaz <marvinthomaz@gmail.com>
2017-01-18 17:47:58 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-/*
-* Verificação e teste do JSLint
-*/
+//Verificação e teste do JSLint
 
-/*
-* Teste de modo estrito
-*/
+//Teste de modo estrito
 
 function estrito() {
   "user strict";
+}
+
+//Obtendo o objeto do contexto global
+
+var global = (function () {
+  return this;
+}());
+
+
+//Declaração de variáveis em uma única linha
+
+function variaveis() {
+  var a = 0,
+      b = a,
+      c = 2 + 2,
+      d = b - c,
+      e = new Object(),
+      f = [],
+      g = {},
+      h;
+  
+  //corpo da função...
 }
